3b1f79fa660d82bf9bebadc932c1e7f6c6a6e7dd
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-09-21 22:35:53 +0300

    Create Package.h

---
+++ Package.h
@@ -0,0 +1,30 @@
+#ifndef UNTITLED_PACKAGE_H
+#define UNTITLED_PACKAGE_H
+using namespace std;
+class Package
+{
+protected:
+    float price;
+    float size;
+    char *name = nullptr;
+public:
+    //конструкторы по умолчанию
+    Package();
+    Package(const char* newName, const float& newSize, const float& newPrice);
+    Package(const char* newName, const float& newSize);
+    Package(const char* newName);
+    //конструктор копирования
+    Package(const Package &ref);
+    //деструктор
+    ~Package();
+    // методы класса
+    void setInfo(const char* newName, const float& newSize, const float& newPrice);
+    void setName(const char* newName);
+    void setSize(const float& newSize);
+    void setPrice(const float& newPrice);
+    const char* getName() const;
+    const float& getSize() const;
+    const float& getPrice() const;
+};
+
+#endif //UNTITLED_PACKAGE_H
