c0deb155eed9fa58aa643e2f677df5642bfb579b
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-09-27 04:27:35 +0300

    Update Package.cpp

--- Package.cpp
+++ Package.cpp
@@ -4,26 +4,29 @@
 #define DEBUG 0
 #define maxLen 10
 #define maxFloat 1000
-#define minFloat 0
+#define minFloat 1
 using namespace std;
 Package::Package() {
     price=0;
     size=0;
 }
-
+bool check(const float& n){
+    if(n>=minFloat&n<maxFloat) return true;
+    else return false;
+}
 Package::Package(const char *newName, const float &newSize, const float &newPrice) {
     if(strlen(strdup(newName))<=maxLen)name = strdup(newName);
     else name= nullptr;
-    if(newSize>minFloat&newSize<maxFloat)size = newSize;
+    if(check)size = newSize;
     else size=0;
-    if(newPrice>minFloat&newPrice<maxFloat)price = newPrice;
+    if(check)price = newPrice;
     else price=0;
 }
 
 Package::Package(const char *newName, const float &newSize) {
     if(strlen(strdup(newName))<=maxLen)name = strdup(newName);
     else name= nullptr;
-    if(newSize>minFloat&newSize<maxFloat)size = newSize;
+    if(check)size = newSize;
     else size=0;
     price=0;
 }
@@ -50,9 +53,9 @@ Package::~Package() {
 void Package::setInfo(const char *newName, const float &newSize, const float &newPrice) {
     if(strlen(strdup(newName))<=maxLen)name = strdup(newName);
     else name= nullptr;
-    if(newSize>minFloat&newSize<maxFloat)size = newSize;
+    if(check)size = newSize;
     else size=0;
-    if(newPrice>minFloat&newPrice<maxFloat)price = newPrice;
+    if(check)price = newPrice;
     else price=0;
 }
 
@@ -62,12 +65,12 @@ void Package::setName(const char *newName) {
 }
 
 void Package::setSize(const float &newSize) {
-    if(newSize>minFloat&newSize<maxFloat)size = newSize;
+    if(check)size = newSize;
     else size=0;
 }
 
 void Package::setPrice(const float &newPrice) {
-    if(newPrice>minFloat&newPrice<maxFloat)price = newPrice;
+    if(check)price = newPrice;
     else price=0;
 }
 
@@ -75,12 +78,12 @@ const char *Package::getName() const {
     if(name!=NULL)return name;
     else return "";
 }
-
 const float &Package::getSize() const { return size;}
 
 const float &Package::getPrice() const { return price;}
 
 const float Package::getValue() const{
-    if(size!=0)return (price/size);
+    if(size>0)return (price/size);
     else return 0;
 };
+
