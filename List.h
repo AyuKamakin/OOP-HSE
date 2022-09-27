356f90c20acb83303de40b58282d33815e4069c3
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-09-27 20:47:16 +0300

    Update List.h

--- List.h
+++ List.h
@@ -10,19 +10,29 @@ public:
 };
 class List
 {
-public:
+protected:
     node * first, * last;
     int len;
+
+public:
     List();;
     List(const List& L);;
     ~List();;
-    void Del(const int pos);
+    void Del(const int pos);;
     void Destruction();;
-    int GetLen() const;;
+
+    const int& GetLen() const;;
     node* GetElem(int pos) const;;
+    node* Head() const;;
+    node* Tail() const;;
+
     void Insert(const Package n,const int pos);;
     void Insert(const Package n);;
     void InsertWSort(const Package n);;
+
     void Print() const;;
     void Print(int pos) const;;
+
+    void Sort() const;;
+    bool IfSorted() const;;
 };
