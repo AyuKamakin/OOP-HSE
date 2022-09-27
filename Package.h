7012edaacb2dcfbaa2d26ae56a66c0e43b86eb6b
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-09-28 02:56:26 +0300

    Update Package.h

--- Package.h
+++ Package.h
@@ -4,28 +4,30 @@ using namespace std;
 class Package
 {
 protected:
-    float price;
-    float size;
+    int32_t price;
+    int32_t size;
     char *name = nullptr;
 public:
     //конструкторы по умолчанию
     Package();
-    Package(const char* newName, const float& newSize, const float& newPrice);
-    Package(const char* newName, const float& newSize);
+    Package(const char* newName, const int32_t& newSize, const int32_t& newPrice);
+    Package(const char* newName, const int32_t& newSize);
     Package(const char* newName);
     //конструктор копирования
     Package(const Package &ref);
     //деструктор
     ~Package();
     // методы класса
-    void setInfo(const char* newName, const float& newSize, const float& newPrice);
+    void setInfo(const char* newName, const int32_t& newSize, const int32_t& newPrice);
     void setName(const char* newName);
-    void setSize(const float& newSize);
-    void setPrice(const float& newPrice);
+    void setSize(const int32_t& newSize);
+    void setPrice(const int32_t& newPrice);
     const char* getName() const;
-    const float& getSize() const;
-    const float& getPrice() const;
+    const int32_t& getSize() const;
+    const int32_t& getPrice() const;
     const float getValue() const;
+    void readFromF(const string filename);
+    void writeToF(const string filename) const;
 };
 
 #endif //UNTITLED_PACKAGE_H
