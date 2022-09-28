121ec386ee32ddb73f13425ae4a1d2949a934bb6
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-09-29 02:47:37 +0300

    Update List.h

--- List.h
+++ List.h
@@ -18,24 +18,23 @@ public:
     List();;
     List(const List& L);;
     ~List();;
-    void Del(const int pos);;
+    void Del(const int& pos);;
     void Destruction();;
 
     const int& GetLen() const;;
-    node* GetElem(int pos) const;;
     node* Head() const;;
     node* Tail() const;;
+    node* GetElem(const int& pos) const;;
 
-    void Insert(const Package n,const int pos);;
-    void Insert(const Package n);;
-    void InsertWSort(const Package n);;
+    void Insert(const Package& n);;
+    void InsertBetween(const Package& n, const int& pos1, const int& pos2);;
+    void InsertWSort(const Package& n);;
 
-    void FromFile(const string filename);;
-    void ToFile(const string filename);;
+    void FromFile(const string& filename);;
+    void ToFile(const string& filename);;
 
 
-    void Print() const;;
-    void Print(int pos) const;;
+    void Print(const int& pos) const;;
 
     void Sort() const;;
     bool IfSorted() const;;
