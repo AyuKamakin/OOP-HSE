070c3c794d001c8984940f4237a699ddd292348d
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-10-04 08:25:04 +0300

    Update List.h

--- List.h
+++ List.h
@@ -1,11 +1,33 @@
 #include <iostream>
 #include "Package.h"
-#include "Node.h"
 #define UNTITLED_LIST_H
 using namespace std;
 
 class List
 {
+public: class Node {
+        friend class Package;
+    protected:
+        Node* innext= nullptr;
+        Node* inprev= nullptr;
+        Package data;
+    public:
+        Node();
+        Node(const Package& temp);
+        Node(const Package& temp, Node* next, Node* prev);
+        Node(Node* next, Node* prev);
+        ~Node();
+        Node* next() const;
+        Node* prev() const;
+        void setNext(Node* n);;
+        void setPrev(Node* n);;
+        void setNextNull();;
+        void setPrevNull();;
+        void setPack(const Package& n);
+        const Package& getPack();
+        const bool ifNext();
+        const bool ifPrev();
+    };
 protected:
     Node* first;
     Node* last;
