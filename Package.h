34cc0ef6b0700ff6c51698d70b98d04f43f46c96
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-10-15 08:50:16 +0300

    Update Package.h

--- Package.h
+++ Package.h
@@ -10,6 +10,7 @@ protected:
     int32_t size;
     float value=0;
     char *name = nullptr;
+    void innerSetValue();
 public:
     //конструкторы по умолчанию
     Package();
@@ -20,17 +21,17 @@ public:
     Package(const Package &ref);
     //деструктор
     ~Package();
-    // методы класса
+
+    virtual // методы класса
     void setInfo(const char* newName, const int32_t& newSize, const int32_t& newPrice);
     void setName(const char* newName);
-    void setSize(const int32_t& newSize);
-    void setPrice(const int32_t& newPrice);
     const char* getName() const;
     const int32_t& getSize() const;
     const int32_t& getPrice() const;
     const float& getValue() const;
-    void innerSetValue();
     void Print() const;;
+
+    virtual const bool type()const ;
 };
 
 
