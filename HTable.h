ce0249ec44decd12af8465d49500b02e783ff491
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-11-22 21:54:13 +0300

    Update HTable.h

--- HTable.h
+++ HTable.h
@@ -23,16 +23,62 @@ public:
         table.resize(STARTSIZE);
     };
 
-    HTable(const HTable &other) {};
+    HTable(const HTable &other) {
+        table.resize(other.table.size());
+        hashConst = other.hashConst;
+        int i = 0;
+        sum = 0;
+        count = 0;
+        while (i < other.table.size()) {
+            if (other.table[i] != nullptr) {
+                //insert(other.table[i]->key,other.table[i]->value);
+                auto *newNode = new hList;
+                newNode->key = other.table[i]->key;
+                newNode->value = other.table[i]->value;
+                sum += other.table[i]->value;
+                count++;
+                table[i] = newNode;
+                if (other.table[i]->next != nullptr) {
+                    auto ref = other.table[i];
+                    auto prev = newNode;
+                    while (ref->next != nullptr) {
+                        ref = ref->next;
+                        auto *nNode = new hList;
+                        nNode->key = ref->key;
+                        nNode->value = ref->value;
+                        sum += nNode->value;
+                        count++;
+                        prev->next = nNode;
+                        prev = nNode;
+                    }
+                } else newNode->next = nullptr;
+            }
+            i++;
+        }
+    } //готово
 
     ~HTable() {
-
-    }
-
-    void clearTable() {
+        clearTable();
         table.clear();
-        count = 0;
-    }
+    } // деструктор
+    void clearTable() {
+        for (int i = 0; i < table.size(); i++) {
+            while (table[i] != nullptr) {
+                auto curr = table[i];
+                hList *prev = nullptr;
+                while (curr->next != nullptr) {
+                    prev = curr;
+                    curr = curr->next;
+                }
+                count--;
+                sum = sum - curr->value;
+                if (prev != nullptr) prev->next = nullptr;
+                else if (curr == table[i]) table[i] = nullptr;
+                delete curr;
+                curr = nullptr;
+            }
+        }
+    } //очиститель содержимого
 
     void contentsCheck() {
         if (count > table.size() / 2) {
@@ -49,7 +95,7 @@ public:
 
     const int getPos(K key) {
         return hashPos(key);
-    }
+    } //готово
 
     const bool inTable(K nkey) {
         if (table[hashPos(nkey)] != nullptr) {
@@ -65,7 +111,7 @@ public:
             }
         } else return false;
     }// готово
-    const bool inTable(K nkey, int pos) {
+    /*const bool inTable(K nkey, int pos) {
         if (table[pos] != nullptr) {
             if (table[pos]->key == nkey) {
                 return true;
@@ -78,11 +124,11 @@ public:
                 else return false;
             }
         } else return false;
-    } //готово
+    } */
     const bool isEmpty(int pos) {
         if (table[pos] == nullptr) return true;
         else return false;
-    }
+    }//готово
 
     const int hashPos(K key) const {
         return abs(static_cast<int>(std::hash<K>()(key) % (STARTSIZE * hashConst)));
@@ -97,6 +143,7 @@ public:
         if (table[pos] == nullptr) {
             table[pos] = newNode;
             count++;
+            contentsCheck();
             sum += data;
             return pos;
         } else if (table[pos]->key == nkey) {
@@ -113,11 +160,13 @@ public:
             if (curr->next == nullptr && curr->key != nkey) {
                 curr->next = newNode;
                 sum += data;
+                count++;
+                contentsCheck();
                 return 2;
             }
         }
     } // готово
-    int insert(K nkey, V data, int pos) {
+    int insertByPos(K nkey, V data, int pos) {
         auto *newNode = new hList;
         newNode->value = data;
         newNode->key = nkey;
@@ -144,9 +193,7 @@ public:
                 return 2;
             }
         }
-    } // готово
-
-
+    }
 
     int8_t removePair(K nkey) {
         int pos = hashPos(nkey);
@@ -167,13 +214,14 @@ public:
                     if (prev != nullptr) prev->next = curr->next;
                     else table[pos] = curr->next;
                 }
+                sum = sum - curr->data;
                 delete curr;
                 count--;
                 return 1;
             }
         }
     }//готово
-    int8_t removePair(K nkey, int pos) {
+    int8_t removePairByPos(K nkey, int pos) {
         if (table[pos] == nullptr) {
             return 0;
         } else {
@@ -191,6 +239,7 @@ public:
                     if (prev != nullptr) prev->next = curr->next;
                     else table[pos] = curr->next;
                 }
+                sum = sum - curr->data;
                 delete curr;
                 count--;
                 return 1;
@@ -204,7 +253,7 @@ public:
     const int getSum() const {
         return sum;
     }// готово
-
+/*
     const V &getV(K nkey) const {
         int pos = hashPos(nkey);
         if (table[pos]->key == nkey) {
@@ -219,7 +268,7 @@ public:
             }
         }
     }// готово
-
+    */
     const V &getVbypos(int pos, K nkey) {
         if (table[pos]->key == nkey) {
             return table[pos]->value;
@@ -232,6 +281,40 @@ public:
             else return 0;
         }
     }// готово
+
+    V &operator[](const K &nkey) {
+        int pos = hashPos(nkey);
+        if (table[pos] != nullptr) {
+            auto curr = table[pos];
+            while (curr->next != nullptr) {
+                if (curr->key == nkey) {
+                    return curr->value;
+                }
+                curr = curr->next;
+            }
+            if (curr->key != nkey) {
+                throw std::exception();
+            }
+        }
+        throw std::exception();
+    }
+
+    bool operator==(const HTable &other) {
+       if (count == other.count && table.size() == other.table.size() && sum == other.sum) {
+            for(int i=0;i<table.size();i++) {
+                if (table[i] != nullptr) {
+                    auto curr = table[i];
+                    while (curr != nullptr) {
+                   if (other.table[hashPos(table[i]->key)] == nullptr ||
+                      other.table[hashPos(table[i]->key)]->value != table[i]->value) return false;
+                     curr=curr->next;
+                     }
+                }
+          }
+            return true;
+        } else return false;
+    } // готово
+
 public:
     vector<hList *> table;
     std::size_t count;
