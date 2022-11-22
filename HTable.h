91952eb805e31a4814801115857c5b4f4c8e2f3b
AyuKamakin <101467421+AyuKamakin@users.noreply.github.com>
2022-11-22 05:05:27 +0300

    Update HTable.h

--- HTable.h
+++ HTable.h
@@ -11,8 +11,8 @@ template<typename K, typename V>
 class HTable {
 public:
     struct hList {
-        V *value = nullptr;
-        K *key = nullptr;
+        V value;
+        K key;
         hList *next = nullptr;
     };
 
@@ -38,152 +38,200 @@ public:
         if (count > table.size() / 2) {
             table.resize(table.size() * 10);
             hashConst = hashConst * 10;
-            for (int i = 0; i < table.size / 10; i++) {
+            for (int i = 0; i < table.size() / 10; i++) {
                 if (table[i] != nullptr) {
-                    table[hashPos(*(table[i]->key))] = table[i];
+                    table[hashPos(table[i]->key)] = table[i];
                     table[i] = nullptr;
                 }
             }
-            return;
         } else return;
+    } //готово
+
+    const int getPos(K key) {
+        return hashPos(key);
+    }
+
+    const bool inTable(K nkey) {
+        if (table[hashPos(nkey)] != nullptr) {
+            if (table[hashPos(nkey)]->key == nkey) {
+                return true;
+            } else {
+                auto curr = table[hashPos(nkey)];
+                while (curr->key != nkey && curr->next != nullptr) {
+                    curr = curr->next;
+                }
+                if (curr->key == nkey) return true;
+                else return false;
+            }
+        } else return false;
+    }// готово
+    const bool inTable(K nkey, int pos) {
+        if (table[pos] != nullptr) {
+            if (table[pos]->key == nkey) {
+                return true;
+            } else {
+                auto curr = table[pos];
+                while (curr->key != nkey && curr->next != nullptr) {
+                    curr = curr->next;
+                }
+                if (curr->key == nkey) return true;
+                else return false;
+            }
+        } else return false;
+    } //готово
+    const bool isEmpty(int pos) {
+        if (table[pos] == nullptr) return true;
+        else return false;
     }
 
     const int hashPos(K key) const {
         return abs(static_cast<int>(std::hash<K>()(key) % (STARTSIZE * hashConst)));
-    }
+    } //готово
 
     int insert(K nkey, V data) {
         int pos = hashPos(nkey);
         auto *newNode = new hList;
-        auto *dat = new V;
-        auto *k = new K;
-        *k = nkey;
-        *dat = data;
-        newNode->value = dat;
-        newNode->key = k;
+        newNode->value = data;
+        newNode->key = nkey;
         newNode->next = nullptr;
         if (table[pos] == nullptr) {
             table[pos] = newNode;
             count++;
             sum += data;
             return pos;
-        } else if (*(table[pos]->key) == nkey) {
+        } else if (table[pos]->key == nkey) {
             delete newNode;
-            delete dat;
-            delete k;
             return 0;
         } else {
             auto curr = table[pos];
             while (curr->next != nullptr) {
-                if (*(curr->key) == nkey) {
+                if (curr->key == nkey) {
                     delete newNode;
                     return 0;
                 } else curr = curr->next;
             }
-            if (curr->next == nullptr && *(curr->key) != nkey) {
+            if (curr->next == nullptr && curr->key != nkey) {
                 curr->next = newNode;
                 sum += data;
                 return 2;
             }
         }
-    }
-
-    int8_t inserttest(K nkey, V data, const int pos) {
-
+    } // готово
+    int insert(K nkey, V data, int pos) {
         auto *newNode = new hList;
-        auto *dat = new V;
-        auto *k = new K;
-        *k = nkey;
-        *dat = data;
-        newNode->value = dat;
-        newNode->key = k;
+        newNode->value = data;
+        newNode->key = nkey;
         newNode->next = nullptr;
         if (table[pos] == nullptr) {
             table[pos] = newNode;
             count++;
             sum += data;
             return pos;
-        } else if (*(table[pos]->key) == nkey) {
+        } else if (table[pos]->key == nkey) {
             delete newNode;
-            delete dat;
-            delete k;
             return 0;
         } else {
             auto curr = table[pos];
             while (curr->next != nullptr) {
-                if (*(curr->key) == nkey) {
+                if (curr->key == nkey) {
                     delete newNode;
-                    delete dat;
-                    delete k;
                     return 0;
                 } else curr = curr->next;
             }
-            if (curr->next == nullptr && *(curr->key) != nkey) {
+            if (curr->next == nullptr && curr->key != nkey) {
                 curr->next = newNode;
                 sum += data;
                 return 2;
             }
         }
-    }
+    } // готово
+
+
 
     int8_t removePair(K nkey) {
         int pos = hashPos(nkey);
         if (table[pos] == nullptr) {
-            return 1;
+            return 0;
         } else {
             auto curr = table[pos];
-            while (curr->next != nullptr) {
-                if (*(table[pos]->key) == nkey) {
-                    if (table[pos]->next == nullptr) {
-                        delete table[pos];
-                        table[pos] = nullptr;
-                    } else {
-                        auto next = table[pos]->next;
-                        delete table[pos];
-                        table[pos] = next;
-                    }
-                    count--;
-                    return 0;
+            hList *prev = nullptr;
+            while (curr->next != nullptr && curr->key != nkey) {
+                prev = curr;
+                curr = curr->next;
+            }
+            if (curr->key == nkey) {
+                if (curr->next == nullptr) {
+                    if (prev != nullptr) prev->next = nullptr;
+                    else table[pos] = nullptr;
+                } else {
+                    if (prev != nullptr) prev->next = curr->next;
+                    else table[pos] = curr->next;
                 }
+                delete curr;
+                count--;
+                return 1;
             }
         }
-    }
-
+    }//готово
+    int8_t removePair(K nkey, int pos) {
+        if (table[pos] == nullptr) {
+            return 0;
+        } else {
+            auto curr = table[pos];
+            hList *prev = nullptr;
+            while (curr->next != nullptr && curr->key != nkey) {
+                prev = curr;
+                curr = curr->next;
+            }
+            if (curr->key == nkey) {
+                if (curr->next == nullptr) {
+                    if (prev != nullptr) prev->next = nullptr;
+                    else table[pos] = nullptr;
+                } else {
+                    if (prev != nullptr) prev->next = curr->next;
+                    else table[pos] = curr->next;
+                }
+                delete curr;
+                count--;
+                return 1;
+            }
+        }
+    }// готово
     const int getCount() const {
         return count;
-    }
+    }// готово
 
     const int getSum() const {
         return sum;
-    }
+    }// готово
 
     const V &getV(K nkey) const {
         int pos = hashPos(nkey);
-        if (*(table[pos]->key) == nkey) {
-            return *(table[pos]->value);
+        if (table[pos]->key == nkey) {
+            return table[pos]->value;
         } else {
             auto curr = table[pos];
             while (curr->next != nullptr) {
-                if (*(curr->key) == nkey) {
-                    return *(curr->value);
+                if (curr->key == nkey) {
+                    return curr->value;
                 }
                 curr = curr->next;
             }
         }
-    }
+    }// готово
 
     const V &getVbypos(int pos, K nkey) {
-        if (*(table[pos]->key) == nkey) {
-            return *(table[pos]->value);
+        if (table[pos]->key == nkey) {
+            return table[pos]->value;
         } else {
             auto curr = table[pos];
-            while(*(curr->key)!=nkey&&curr->next!= nullptr){
-                curr=curr->next;
+            while (curr->key != nkey && curr->next != nullptr) {
+                curr = curr->next;
             }
-            if(*(curr->key)==nkey)  return *(curr->value);
+            if (curr->key == nkey) return curr->value;
             else return 0;
         }
-    };
+    }// готово
 public:
     vector<hList *> table;
     std::size_t count;
